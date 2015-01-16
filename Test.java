24c91e3f4130224508daf888e3a154745fa45eb0
Travis Brown <tbrown@twitter.com>
2015-01-16 12:07:18 -0500

    Minimized configuration

---
+++ Test.java
@@ -0,0 +1,6 @@
+public class Test {
+  public static void main(String[] args) throws java.net.UnknownHostException {
+    System.out.println(java.net.InetAddress.getLocalHost().getHostName());
+  }
+}
+
